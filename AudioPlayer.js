80a84188898460bfd8b4937335329f231412e97c
Damien Goujard <damien@actigraph.com>
2015-11-04 15:03:52 +0100

    fix probléme sous android de chargement

--- AudioPlayer.js
+++ AudioPlayer.js
@@ -2,9 +2,11 @@
 
 var RNAudioPlayer = require('react-native').NativeModules.RNAudioPlayer;
 var NativeAppEventEmitter = require('react-native').NativeAppEventEmitter;
+var { Platform } = require('react-native');
 
 module.exports = class AudioPlayer {
   play(fileName: string) {
+    fileName = Platform.OS === 'ios' ? fileName : fileName.replace(/\.[^/.]+$/, "");
     RNAudioPlayer.play(fileName);
   }
 
